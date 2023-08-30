d61f4ca36a361b8478682d25c7ef2a2a451c8fe8
Serlle Rosale <serlle@caraji.io>
2023-08-29 21:08:30 -0600

    Added home page root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "fruits#export_csv", as: :export_fruits_csv
+  root 'pages#home'
 end
