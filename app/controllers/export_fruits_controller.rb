444878220922fa6b54d9d27badfdfa82612eb075
Serlle Rosale <serlle@caraji.io>
2023-08-31 16:30:01 -0600

    Added validation in the controller of fruits

--- app/controllers/export_fruits_controller.rb
+++ app/controllers/export_fruits_controller.rb
@@ -5,6 +5,28 @@ class ExportFruitsController < ApplicationController
   end
 
   def create
+    if all_params_empty?
+      redirect_to new_export_fruit_path, alert: "Please provide at least one parameter."
+    elsif name_zip_params_empty?
+      redirect_to new_export_fruit_path, alert: "Please provide a file name."
+    else
+      service = FruitsApiService.new(fruit_params)
+      @fruits = service.fetch_fruits
+    end
+  end
+
+  private
+
+  def fruit_params
+    params.permit(:family, :genus, :order, :name_zip, :name_fruit)
+  end
+
+  def all_params_empty?
+    fruit_params.values.all?(&:blank?)
+  end
+
+  def name_zip_params_empty?
+    fruit_params[:name_zip].blank?
   end
 end
 
